0ebcea1a0b456ab2415597db2cf6ab395b8bf4c3
smg <smengar05@gmail.com>
2019-09-24 08:36:04 +0200

    0.90007 Añadir al menu Autorizados

---
+++ app/src/main/java/com/epalburquerqueiii/aexperience/UI/Autorizados/AutorizadosFragment.kt
@@ -0,0 +1,80 @@
+package com.epalburquerqueiii.aexperience.UI.Autorizados
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.epalburquerqueiii.aexperience.Data.Adapter.AutorizadosAdapter
+import com.epalburquerqueiii.aexperience.Data.Model.Autorizado
+import com.epalburquerqueiii.aexperience.R
+import kotlinx.android.synthetic.main.list_fragment.*
+
+
+class AutorizadosFragment : Fragment() {
+
+
+    private lateinit var viewModel: AutorizadosViewModel
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var mAdapter: AutorizadosAdapter
+
+    private val Crear = 0
+    private val Editar = 1
+
+    override fun onCreateView(
+
+        inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupViewModelAndObserve()
+        viewModel.getRegistros()
+        bt_new.setOnClickListener{
+            var intent = Intent(activity, AutorizadoActivity::class.java)
+            var registro = Autorizado(0,0,"","")
+            intent.putExtra("registro",registro)
+            intent.putExtra("modo", Crear)
+            startActivity(intent) }
+    }
+
+    fun setupViewModelAndObserve() {
+
+        viewModel = ViewModelProvider(this).get(AutorizadosViewModel::class.java)
+        // TODO: Use the ViewModel
+        val registrosObserver = Observer<ArrayList<Autorizado>> {
+
+            mAdapter = AutorizadosAdapter(it, activity!!)
+            linearLayoutManager = LinearLayoutManager(activity)
+
+            recycleview.layoutManager = linearLayoutManager
+            recycleview.setHasFixedSize(true)
+            recycleview.adapter = mAdapter
+
+
+            mAdapter.setOnItemClickListener(object : AutorizadosAdapter.OnItemClickListener {
+                override fun onItemClick(registro: Autorizado) {
+                    //Toast.makeText(activity,""+registro.id,Toast.LENGTH_SHORT).show()
+                    var intent = Intent(activity, AutorizadoActivity::class.java)
+
+                    intent.putExtra("registro", registro)
+
+                    intent.putExtra("modo", Editar)
+                    startActivityForResult(intent,1)
+                }
+            })
+
+        }
+        viewModel.getregistrosLiveData().observe(this, registrosObserver)
+   }
+
+}
---
+++ app/src/main/java/com/epalburquerqueiii/aexperience/UI/Autorizados/AutorizadosViewModel.kt
@@ -0,0 +1,69 @@
+package com.epalburquerqueiii.aexperience.UI.Autorizados
+
+import android.util.Log
+import androidx.databinding.ObservableBoolean
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.epalburquerqueiii.aexperience.Data.Model.Autorizado
+import com.epalburquerqueiii.aexperience.Data.Model.Autorizados
+import com.epalburquerqueiii.aexperience.Data.Network.AutorizadosApi
+import com.epalburquerqueiii.aexperience.Data.Network.RetrofitBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class AutorizadosViewModel : ViewModel() {
+
+
+//    private val AutorizadosUseCase = AutorizadosUseCase()
+
+    val changed = ObservableBoolean(false)
+
+
+    private val registros = MutableLiveData<ArrayList<Autorizado>>()
+
+    private fun setRegistros(listregistros:ArrayList<Autorizado>) {
+        registros.value = listregistros
+    }
+
+    fun getRegistros(){
+        setRegistros(getRecords())
+    }
+
+    fun getregistrosLiveData(): LiveData<ArrayList<Autorizado>> {
+        return registros
+    }
+
+    fun getRecords():ArrayList<Autorizado> {
+
+        var datos = ArrayList<Autorizado>()
+
+        val get = RetrofitBuilder.builder().create(AutorizadosApi::class.java)
+        val callget = get.Get()
+        callget.enqueue(object : Callback<Autorizados> {
+            override fun onFailure(call: Call<Autorizados>, t: Throwable) {
+                Log.i("Autorizados Fragment:", "" + t.message)
+            }
+
+            override fun onResponse(call: Call<Autorizados>, response: Response<Autorizados>) {
+
+                @Suppress("NAME_SHADOWING")
+                val response = response.body() as Autorizados
+//                val size = response.Records!!.size
+                datos = response.Records!!
+                setRegistros(datos)
+            }
+        })
+/*
+        if (datos.size == 0) {
+            datos!!.add(Autorizado(0,"No hay registros","","",0,"",""))
+        }
+*/
+        return datos
+    }
+
+    public fun Load()=changed.set(false)
+    public fun make_Change()=changed.set(true)
+
+}
