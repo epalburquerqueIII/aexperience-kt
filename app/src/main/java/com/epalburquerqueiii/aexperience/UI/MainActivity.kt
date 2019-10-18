a3c06b32d2e83872964af8f13183c009997ad470
smg <smengar05@gmail.com>
2019-10-18 14:08:35 +0200

    0.90006 Splash screen

--- app/src/main/java/com/epalburquerqueiii/aexperience/UI/MainActivity.kt
+++ app/src/main/java/com/epalburquerqueiii/aexperience/UI/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.epalburquerqueiii.aexperience.UI
 
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -11,6 +14,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController :NavController
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 4000 //3 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            splash.setVisibility(View.INVISIBLE)
+            // Mediante fragment
+           //replaceFragment(AutorizadosFragment())
+        }
+    }
 
 
 
@@ -25,11 +38,28 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(bottom_nav,navController)
         NavigationUI.setupActionBarWithNavController(this, navController)
 
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
 
     }
 
+    override fun onNavigateUp(): Boolean {
+        return super.onNavigateUp()
+        Thread.sleep(500)
+    }
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController,null)
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction =  supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_fragment, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 
 }
