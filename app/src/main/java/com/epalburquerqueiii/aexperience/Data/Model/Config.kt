6be291e108ba34ebea0347811ae2ba0669b6bebb
epalburquerqueiii <ep.tecnicoinnova@gmail.com>
2019-10-21 12:05:32 +0200

    0.90004 Añado clase config con variable globales

---
+++ app/src/main/java/com/epalburquerqueiii/aexperience/Data/Model/Config.kt
@@ -0,0 +1,8 @@
+package com.epalburquerqueiii.aexperience.Data.Model
+
+class Config {
+    companion object {
+        val base_url:String = "http://192.168.0.3:3000/"
+        val cms_url:String = "http://192.168.0.3:1313/"
+    }
+}
