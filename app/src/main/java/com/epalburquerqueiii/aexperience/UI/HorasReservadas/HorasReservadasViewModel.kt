d526580a496145686a22d32e438dc2bb80f9ec7e
craceror01 <craceror01@gmail.com>
2019-10-21 14:15:06 +0200

    0.220007  Carlos EventosAPI errores sync

--- app/src/main/java/com/epalburquerqueiii/aexperience/UI/HorasReservadas/HorasReservadasViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.epalburquerqueiii.aexperience.UI.HorasReservadas
-
-import androidx.lifecycle.ViewModel
-
-class HorasReservadasViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
-
-
