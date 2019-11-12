1721b4faedd5509e3f5f87914a7ae05888a8ec07
aliciamachadoa <aliciamachadoa@gmail.com>
2019-11-12 14:24:07 +0100

    0.13013 despues sinc

--- app/src/main/java/com/epalburquerqueiii/aexperience/Data/Network/NewslettersApi.kt
+++ app/src/main/java/com/epalburquerqueiii/aexperience/Data/Network/NewslettersApi.kt
@@ -12,19 +12,15 @@ import retrofit2.http.POST
 
 
 interface NewslettersApi {
+    //view
 
-    //List
-    @FormUrlEncoded
-    @POST("newsletter/"+BuildConfig.VIEW_DATA)
-    fun List(/*debe haber un encabezado o un cuerpo*/
-        @Field("X-CSRF-Token") CSRFToken:String
-    ) :Call<Newsletters>
+    @GET("newsletter/"+BuildConfig.VIEW_DATA)
+    fun Get(/*debe haber un encabezado o un cuerpo*/) :Call<Newsletters>
 
     //create
     @FormUrlEncoded
     @POST("newsletter/"+BuildConfig.CREATE_DATA)
     fun Create(
-        @Field("X-CSRF-Token") CSRFToken:String,
         @Field("Email")Email:String,
         @Field("Idtiponoticias")Idtiponoticias:Int
     ):Call<responseModel>
@@ -33,7 +29,6 @@ interface NewslettersApi {
     @FormUrlEncoded
     @POST(BuildConfig.BASE_URL+"newsletter/"+BuildConfig.UPDATE_DATA)
     fun Update(
-        @Field("X-CSRF-Token") CSRFToken:String,
         @Field("Id")id: Int,
         @Field("Email")Email:String,
         @Field("Idtiponoticias")Idtiponoticias:Int
@@ -43,31 +38,30 @@ interface NewslettersApi {
     @FormUrlEncoded
     @POST(BuildConfig.BASE_URL+"newsletter/"+BuildConfig.DELETE_DATA)
     fun Delete(
-        @Field("X-CSRF-Token") CSRFToken:String,
         @Field("Id")id:Int
+
     ):Call<responseModel>
 
     //SaveNewsletterUser
     @FormUrlEncoded
     @POST(BuildConfig.BASE_URL+"newsletter/newsletterguardar")
     fun SaveNewsletterUser(
-        @Field("X-CSRF-Token") CSRFToken:String,
         @Field("Id")id: Int,
         @Field("Email")Email:String,
-        @Field("nwch1")ch1:Int,
-        @Field("nwch2")ch2:Int,
-        @Field("nwch3")ch3:Int,
-        @Field("nwch4")ch4:Int,
-        @Field("nwch5")ch5:Int,
-        @Field("nwch6")ch6:Int,
-        @Field("nwch7")ch7:Int,
-        @Field("nwch8")ch8:Int,
-        @Field("nwch9")ch9:Int,
-        @Field("nwch10")ch10:Int
+        @Field("nwch1")nwch1:Int,
+        @Field("nwch2")nwch2:Int,
+        @Field("nwch3")nwch3:Int,
+        @Field("nwch4")nwch4:Int,
+        @Field("nwch5")nwch5:Int,
+        @Field("nwch6")nwch6:Int,
+        @Field("nwch7")nwch7:Int,
+        @Field("nwch8")nwch8:Int,
+        @Field("nwch9")nwch9:Int,
+        @Field("nwch10")nwch10:Int
     ):Call<responseModel>
 
     @FormUrlEncoded
-    @POST("newsletter/getoptions"+BuildConfig.GETOPTIONS_DATA)
+    @POST("newsletter/"+BuildConfig.GETOPTIONS_DATA)
     fun GetOptions(/*debe haber un encabezado o un cuerpo*/
         @Field("X-CSRF-Token") CSRFToken:String
     ) :Call<Options>
