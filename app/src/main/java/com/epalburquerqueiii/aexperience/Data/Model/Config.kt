ee4160855b753abc4ee17db0f15450d463aae42a
smg <smengar05@gmail.com>
2019-11-14 14:49:15 +0100

    0.900020 direcciones

--- app/src/main/java/com/epalburquerqueiii/aexperience/Data/Model/Config.kt
+++ app/src/main/java/com/epalburquerqueiii/aexperience/Data/Model/Config.kt
@@ -2,7 +2,7 @@ package com.epalburquerqueiii.aexperience.Data.Model
 
 class Config {
     companion object {
-        val base_url:String = "http://192.168.0.3:3000/"
-        val cms_url:String = "http://192.168.0.3:1313/"
+        val base_url:String = "http://192.168.0.82:8088/"
+        val cms_url:String = "http://192.168.0.82:1313/"
     }
 }
