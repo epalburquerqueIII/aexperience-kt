399994a0f8d8ef9694264b4195dd4809e0cc31e4
smg <smengar05@gmail.com>
2019-11-18 13:48:15 +0100

    0.900028 Ajustes presentacion

--- app/src/main/java/com/epalburquerqueiii/aexperience/Data/Model/Config.kt
+++ app/src/main/java/com/epalburquerqueiii/aexperience/Data/Model/Config.kt
@@ -2,7 +2,9 @@ package com.epalburquerqueiii.aexperience.Data.Model
 
 class Config {
     companion object {
+//        val base_url:String = "http://192.168.0.14:8088/"
         val base_url:String = "http://192.168.0.82:8088/"
+//        val cms_url:String = "http://192.168.0.14:1313/"
         val cms_url:String = "http://192.168.0.82:1313/"
     }
 }
